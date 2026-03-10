5b4ab5aec523e787afb17f8315cfe37cf8f2030e
Ben Simms <ben@bensimms.moe>
2026-03-10 18:23:38 +0100

    Push

---
+++ Formula/ghidra@12.0.4.rb
@@ -0,0 +1,37 @@
+class GhidraAT1204 < Formula
+  desc "Multi-platform software reverse engineering framework"
+  homepage "https://github.com/NationalSecurityAgency/ghidra"
+  url "https://github.com/niooss-ledger/ghidra/archive/refs/heads/fix-decompiler-function-pointer-arm-thumb.zip"
+  sha256 "2650b5c5f2615883db347157f88d15f6f06cd14e223156cd6b6034264081f5b4"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^Ghidra[._-]v?(\d+(?:\.\d+)+)(?:[._-]build)?$/i)
+  end
+
+  depends_on "gradle" => :build
+  depends_on "python@3.14" => :build
+  depends_on "openjdk@21"
+
+  def install
+    inreplace "Ghidra/application.properties", "DEV", "PUBLIC" # Mark as a release
+    system "gradle", "-I", "gradle/support/fetchDependencies.gradle"
+
+    system "gradle", "buildNatives"
+    system "gradle", "assembleAll", "-x", "FileFormats:extractSevenZipNativeLibs"
+
+    libexec.install (buildpath/"build/dist/ghidra_#{version}_PUBLIC").children
+    (bin/"ghidraRun").write_env_script libexec/"ghidraRun",
+                                       Language::Java.overridable_java_home_env("21")
+  end
+
+  test do
+    (testpath/"analyzeHeadless").write_env_script libexec/"support/analyzeHeadless",
+                                                  Language::Java.overridable_java_home_env("21")
+    (testpath/"project").mkpath
+    system "/bin/bash", testpath/"analyzeHeadless", testpath/"project",
+                        "HomebrewTest", "-import", "/bin/bash", "-noanalysis"
+    assert_path_exists testpath/"project/HomebrewTest.rep"
+  end
+end
